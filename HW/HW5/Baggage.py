6f579d5855ce48f3fe34db7fb06ace4387e27b6f
nathanjuman <161769504+nathanjuman@users.noreply.github.com>
2025-02-21 23:17:12 -0500

    problem2

--- HW/HW5/Baggage.py
+++ HW/HW5/Baggage.py
@@ -1,10 +1,12 @@
 from stackDS import Stack
 from queueDS import Queue
 
-#purpose: Read the weights from a given filename and return as a list of ints
-#assumptions:  file formatted with all weights on one line separated by spaces
-#inputs: filename is the name of the file with the weights data
-#output: Returns a list of ints containing the weights
+# purpose: Read the weights from a given filename and return as a list of ints
+# assumptions:  file formatted with all weights on one line separated by spaces
+# inputs: filename is the name of the file with the weights data
+# output: Returns a list of ints containing the weights
+
+
 def get_weight_list(filename):
     file = None
     try:
@@ -17,12 +19,12 @@ def get_weight_list(filename):
     return weightslist
 
 
-#purpose: Obtain a number from the user.
-#inputs: prompt - instructions given to the user.
-#output: Returns number entered by user.
+# purpose: Obtain a number from the user.
+# inputs: prompt - instructions given to the user.
+# output: Returns number entered by user.
 def getNumber(prompt):
     response = input(prompt)
-    ans=None
+    ans = None
     while ans == None:
         try:
             ans = float(response)
@@ -31,29 +33,58 @@ def getNumber(prompt):
             response = input(prompt)
     return ans
 
-#purpose: finds the loading order for the aircraft given weights of bags 
-#inputs: weightlist - an int list containing bag weights
+# purpose: finds the loading order for the aircraft given weights of bags
+# inputs: weightlist - an int list containing bag weights
 #        maxweight - the max weight of a container
-#output: Returns a stack containing containers in the aircraft loading order
-def compute_order(weightlist, maxweight):
-    '''TODO: complete this function '''
+# output: Returns a stack containing containers in the aircraft loading order
 
-#purpose: builds a string containing the loading order for the aircraft 
-#inputs: order - a stack with containers in the the aircraft loading order
-#output: Returns a string containing the aircraft loading order
+
+def compute_order(weightlist, maxweight):
+    container = Stack()
+    bag_order = []
+    weight = 0
+
+    for i in range(len(weightlist)):
+        if (weightlist[i] + weight > maxweight) or (weight == maxweight):
+            container.push(bag_order)
+            weight = 0
+            bag_order = []
+            bag_order.append(str(weightlist[i]))
+            weight += weightlist[i]
+        else:
+            bag_order.append(str(weightlist[i]))
+            weight += weightlist[i]
+
+    if len(bag_order) > 0:
+        container.push(bag_order)
+
+    return container    
+
+# purpose: builds a string containing the loading order for the aircraft
+# inputs: order - a stack with containers in the aircraft loading order
+# output: Returns a string containing the aircraft loading order
 def str_order(order):
     order_str = ""
-   '''TODO: complete this function'''
+    order_list = []
+    while order.size() > 0:
+        order_list += order.pop()   
+    order_str += ' '.join(order_list)
     return order_str
-    
+
+
 def main():
-    filename = input("Enter filename: ") #get filename
-    list_of_weights = get_weight_list(filename) #read weights from file into list
-    
+    filename = input("Enter filename: ")  # get filename
+    # read weights from file into list
+    list_of_weights = get_weight_list(filename)
+
     if list_of_weights == None:
         return
-    
-    max_weight = getNumber("Enter container weight: ") #get container weight
-    print("The aircraft unloading order is:") 
-    order = compute_order(list_of_weights, max_weight) #get the aircraft loading order
-    print(str_order(order)) #print loading order
+
+    max_weight = getNumber("Enter container weight: ")  # get container weight
+    print("The aircraft unloading order is:")
+    # get the aircraft loading order
+    order = compute_order(list_of_weights, max_weight)
+    print(str_order(order))  # print loading order
+
+
+main()
